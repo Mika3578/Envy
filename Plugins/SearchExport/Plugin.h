12b0b434a259562f5f98938826c339e2f554e9cb
Mika3578 <58137747+Mika3578@users.noreply.github.com>
2025-01-12 02:58:26 +0100

    Update Plugin.h

--- Plugins/SearchExport/Plugin.h
+++ Plugins/SearchExport/Plugin.h
@@ -6,70 +6,73 @@
 
 #include "SearchExport.h"
 
-// CPlugin
+// CPlugin - This class implements the plugin functionality.
 
 class ATL_NO_VTABLE CPlugin :
-	public CComObjectRootEx<CComMultiThreadModel>,
-	public CComCoClass<CPlugin, &CLSID_Plugin>,
-	public IGeneralPlugin,
-	public ICommandPlugin
+    public CComObjectRootEx<CComMultiThreadModel>,
+    public CComCoClass<CPlugin, &CLSID_Plugin>,
+    public IGeneralPlugin,
+    public ICommandPlugin
 {
 public:
-	CPlugin() :
-		m_nCmdCheck( 0 )
-	{
-	}
+    CPlugin() :
+        m_nCmdCheck(0)  // Initialize command check ID to 0
+    {
+    }
 
-DECLARE_REGISTRY_RESOURCEID(IDR_PLUGIN)
+    DECLARE_REGISTRY_RESOURCEID(IDR_PLUGIN)
 
-BEGIN_COM_MAP(CPlugin)
-	COM_INTERFACE_ENTRY(IGeneralPlugin)
-	COM_INTERFACE_ENTRY(ICommandPlugin)
-END_COM_MAP()
+    BEGIN_COM_MAP(CPlugin)
+        COM_INTERFACE_ENTRY(IGeneralPlugin)
+        COM_INTERFACE_ENTRY(ICommandPlugin)
+    END_COM_MAP()
 
-DECLARE_PROTECT_FINAL_CONSTRUCT()
+    DECLARE_PROTECT_FINAL_CONSTRUCT()
 
-	HRESULT FinalConstruct()
-	{
-		return S_OK;
-	}
+    // FinalConstruct - Perform any final construction tasks
+    HRESULT FinalConstruct()
+    {
+        return S_OK;
+    }
 
-	void FinalRelease()
-	{
-		m_pApplication.Release();
-		m_pUserInterface.Release();
-	}
+    // FinalRelease - Perform any final release tasks
+    void FinalRelease()
+    {
+        m_pApplication.Release();
+        m_pUserInterface.Release();
+    }
 
 protected:
-	CComPtr< IApplication >		m_pApplication;		// Envy application
-	CComPtr< IUserInterface >	m_pUserInterface;	// Envy GUI
-	UINT						m_nCmdCheck;		// Command ID
+    CComPtr<IApplication> m_pApplication;   // Pointer to Envy application
+    CComPtr<IUserInterface> m_pUserInterface; // Pointer to Envy GUI
+    UINT m_nCmdCheck;   // Command ID
 
-	HRESULT Export(IGenericView* pGenericView, LONG nCount);
+    // Export - Export data to GenericView
+    HRESULT Export(IGenericView* pGenericView, LONG nCount);
 
-	// Insert menu item only if no item present
-	void InsertCommand(LPCTSTR szTitle, const LPCWSTR* szMenu, UINT nID);
+    // InsertCommand - Insert a menu item only if no item is present
+    void InsertCommand(LPCTSTR szTitle, const LPCWSTR* szMenu, UINT nID);
 
-// IGeneralPlugin
+    // IGeneralPlugin Interface Methods
 public:
-	STDMETHOD(SetApplication)(
-		/* [in] */ IApplication __RPC_FAR *pApplication);
-	STDMETHOD(QueryCapabilities)(
-		/* [in] */ DWORD __RPC_FAR *pnCaps);
-	STDMETHOD(Configure)();
-	STDMETHOD(OnSkinChanged)();
+    STDMETHOD(SetApplication)(
+        /* [in] */ IApplication __RPC_FAR *pApplication);
+    STDMETHOD(QueryCapabilities)(
+        /* [in] */ DWORD __RPC_FAR *pnCaps);
+    STDMETHOD(Configure)();
+    STDMETHOD(OnSkinChanged)();
 
-// ICommandPlugin
+    // ICommandPlugin Interface Methods
 public:
-	STDMETHOD(RegisterCommands)();
-	STDMETHOD(InsertCommands)();
-	STDMETHOD(OnUpdate)(
-		/* [in] */ UINT nCommandID,
-		/* [out][in] */ TRISTATE __RPC_FAR *pbVisible,
-		/* [out][in] */ TRISTATE __RPC_FAR *pbEnabled,
-		/* [out][in] */ TRISTATE __RPC_FAR *pbChecked);
-	STDMETHOD(OnCommand)(
-		/* [in] */ UINT nCommandID);
+    STDMETHOD(RegisterCommands)();
+    STDMETHOD(InsertCommands)();
+    STDMETHOD(OnUpdate)(
+        /* [in] */ UINT nCommandID,
+        /* [out][in] */ TRISTATE __RPC_FAR *pbVisible,
+        /* [out][in] */ TRISTATE __RPC_FAR *pbEnabled,
+        /* [out][in] */ TRISTATE __RPC_FAR *pbChecked);
+    STDMETHOD(OnCommand)(
+        /* [in] */ UINT nCommandID);
 };
 
 OBJECT_ENTRY_AUTO(__uuidof(Plugin), CPlugin)
